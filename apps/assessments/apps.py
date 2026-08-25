5660d4f59da16e675aac7008b2a25c67dc8a95ea
Arun <aruunsomesh@gmail.com>
2026-08-25 21:09:01 +0530

    Auto-seed questions on startup/migrate and fix auth dispatch

--- apps/assessments/apps.py
+++ apps/assessments/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def auto_seed_questions(sender, **kwargs):
+    try:
+        from apps.assessments.models import Question
+        if Question.objects.count() == 0:
+            from django.core.management import call_command
+            call_command('seed_questions')
+    except Exception:
+        pass
 
 class AssessmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.assessments'
+
+    def ready(self):
+        post_migrate.connect(auto_seed_questions, sender=self)
